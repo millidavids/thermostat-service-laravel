8dd650e67cad4b9d32febbe43b935468683ed133
David Yurek <david.a.yurek@gmail.com>
2016-01-30 11:56:18 -0500

    configurations according to a guide

--- config/cache.php
+++ config/cache.php
@@ -52,7 +52,7 @@ return [
             'servers' => [
                 [
                     'host' => env('MEMCACHED_HOST', '127.0.0.1'),
-                    'port' => env('MEMCACHED_PORT', 11211),
+                    'port' => env('MEMCACHED_PORT', 11211), 
                     'weight' => 100,
                 ],
             ],
